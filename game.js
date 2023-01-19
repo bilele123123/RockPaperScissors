b6a1e77a2b75762c6f7c0ce6251ed640b18cad3e
Thai Le <tple06203@gmail.com>
2023-01-19 01:22:38 -0800

    Set up basic js functions.

--- game.js
+++ game.js
@@ -1,47 +1,87 @@
-function getComputerChoice () {
-    const min = 1, max = 3;
-    let choiceGenerator = Math.floor(Math.random() * (max - min + 1)) + min;
-    let choice = "";
-    if (choiceGenerator == 1)
+function getComputerChoice () 
+{
+    let choiceGenerator = Math.floor(Math.random() * 3);
+
+    if (choiceGenerator == 0)
     {
-        choice = "Rock";
-        return choice;
+        console.log("Computer picked: Rock");
+        return "rock";
+    }
+    else if (choiceGenerator == 1)
+    {
+        console.log("Computer picked: Paper");
+        return "paper";
     }
     else if (choiceGenerator == 2)
     {
-        choice = "Paper";
-        return choice;
-    }
-    else if (choiceGenerator == 3)
-    {
-        choice = "Scissors";
-        return choice;
+        console.log("Computer picked: Scissors");
+        return "scissors";
     }
 }
 
-function playRound(playerSelection, computerSelection) {
+function playerChoice() 
+{
+    return prompt("Rock, Paper, Scissors: ");
+}
 
-    playerSelection = "Rock";
+function playRound(playerSelection, computerSelection) 
+{
+    playerSelection = playerChoice().toLowerCase();
     computerSelection = getComputerChoice();
 
-    if (playerSelection == computerSelection) 
+    if (playerSelection == "rock")
     {
-        return "The game is a draw!";
+        if (computerSelection == "rock")
+        {
+            console.log("The game is a draw!")
+        }
+        else if (computerSelection == "paper")
+        {
+            console.log("You Lose! Paper beats Rock.")
+        }
+        else
+        {
+            console.log("You Win! Rock beats Scissors.")
+        }
     }
-    else if (playerSelection == "Rock" && computerSelection == "Paper")
+
+    else if (playerSelection == "paper")
     {
-        return "You Lose! " + computerSelection + " beats " + playerSelection;
+        if (computerSelection == "rock")
+        {
+            console.log("You Win! Paper beats Rock.")
+        }
+        else if (computerSelection == "paper")
+        {
+            console.log("This game is a draw!")
+        }
+        else
+        {
+            console.log("You Lose! Scissors beat Paper.")
+        }
     }
-    else if (playerSelection == "Rock" && computerSelection == "Scissors")
+
+    else
     {
-        return "You Won! " + playerSelection + " beats " + computerSelection; 
-    }
-    else if (playerSelection == "Paper" && computerSelection == "Rock")
-    {
-        return "You Won! " + playerSelection + " beats " + computerSelection; 
+        if (computerSelection == "rock")
+        {
+            console.log("You Lose! Rock beats Scissors")
+        }
+        else if (computerSelection == "paper")
+        {
+            console.log("You Win! Scissors Beats Paper")
+        }
+        else
+        {
+            console.log("This game is a draw!")
+        }
     }
 }
 
-function game() {
-
+function game() 
+{
+    for (let i = 0; i < 3; i++)
+    {
+        playRound(playerChoice().toLowerCase(), getComputerChoice());
+    }
 }
